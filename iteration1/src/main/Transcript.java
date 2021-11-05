74d96b81645e271e26a32df2168dc652a1408824
cemanaral <cemanaral425@gmail.com>
2021-11-05 22:48:08 +0300

    Implemented Transcript.java

--- iteration1/src/main/Transcript.java
+++ iteration1/src/main/Transcript.java
@@ -1,10 +1,44 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Transcript {
 	
-	
-	public void calculateGpa(float f) {
-		
+	private HashMap<Course, String> coursesTaken = new HashMap(); // Course -> String(lettergrade) , Course -> String(lettergrade), Course -> String(lettergrade)
+
+
+
+	public float calculateGpa() {
+		Map<String, Double> gradeMap = new HashMap<>();
+		gradeMap.put("AA", 4.00);
+		gradeMap.put("BA", 3.50);
+		gradeMap.put("BB", 3.00);
+		gradeMap.put("CB", 2.50);
+		gradeMap.put("CC", 2.00);
+		gradeMap.put("DC", 1.50);
+		gradeMap.put("DD", 1.00);
+		gradeMap.put("FD", 0.50);
+		gradeMap.put("FF", 0.00);
+
+		int totalCredits = 0;
+		float totalPoints = 0;
+		for (HashMap.Entry<Course, String> pair : this.coursesTaken.entrySet()) {
+			Course course = pair.getKey();
+			String letterGrade = pair.getValue();
+
+			totalPoints += gradeMap.get(letterGrade) * course.getCredit();
+			totalCredits += course.getCredit();
+		}
+
+		return (float)totalPoints / totalCredits;
+
+
+	}
+
+	public void addCourseAndLetterGrade(Course course, String letterGrade) {
+		this.coursesTaken.put(course, letterGrade);
 	}
 
 }
