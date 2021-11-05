3cf85d6ae8677ca399e6a50c8a3998f8f2d5b4ae
haldun halil olcay <81256755+holcay92@users.noreply.github.com>
2021-11-05 17:43:03 +0300

    Update and rename essentialCourses.java to CompulsoryCourse.java

---
+++ iteration1/src/main/CompulsoryCourse.java
@@ -0,0 +1,10 @@
+package studentRegistirationSystem;
+
+public class CompulsoryCourse extends Course{
+
+    public CompulsoryCourse(String courseCode , String name , int credit) {
+        super(courseCode , name , credit);
+
+    }
+
+}
