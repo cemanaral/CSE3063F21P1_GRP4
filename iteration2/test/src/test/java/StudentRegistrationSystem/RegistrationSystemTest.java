98ddfaf36c8f282bb06583ca1de83211450b7c7f
cemanaral <cemanaral425@gmail.com>
2021-12-30 21:44:01 +0300

    added JUnit

---
+++ iteration2/test/src/test/java/StudentRegistrationSystem/RegistrationSystemTest.java
@@ -0,0 +1,12 @@
+package StudentRegistrationSystem;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegistrationSystemTest {
+
+    @org.junit.jupiter.api.Test
+    public void checkCreditLimitExceeds() {
+        assertEquals(true, false, "test mesajım");
+
+    }
+}
