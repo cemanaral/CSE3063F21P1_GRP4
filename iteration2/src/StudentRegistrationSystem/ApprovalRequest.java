049b4beb54968c61f8a68dbad688955512911c27
cemanaral <cemanaral425@gmail.com>
2021-12-31 03:10:09 +0300

    javadoc for ApprovalRequest.java

--- iteration2/src/StudentRegistrationSystem/ApprovalRequest.java
+++ iteration2/src/StudentRegistrationSystem/ApprovalRequest.java
@@ -2,22 +2,17 @@ package StudentRegistrationSystem;
 
 import java.util.ArrayList;
 
+/**
+ * This class is used for encapsulating
+ * courses a Student wants to enroll.
+ */
 public class ApprovalRequest {
-	// Attributes
 	private boolean isApproved;
 	private ArrayList <Course> courses = new ArrayList<Course>();
-
-
-
-	// private Schedule schedule = new Schedule();
-	
-	
-	//GETTERS AND SETTERS
 
 	public boolean isApproved() {
 		return isApproved;
 	}
-
 	public void setApproved(boolean isApproved) {
 		this.isApproved = isApproved;
 	}
@@ -27,20 +22,18 @@ public class ApprovalRequest {
 	public void setCourses(ArrayList<Course> courses) {
 		this.courses = courses;
 	}
-//	public Schedule getSchedule() {
-//		return schedule;
-//	}
-//	public void setSchedule(Schedule schedule) {
-//		this.schedule = schedule;
-//	}
 
+	/**
+	 * Adds a course to ApprovalRequest
+	 *
+	 * @param course
+	 * @return false if course was already added else true
+	 */
 	public boolean addCourse(Course course) {
 		if (this.courses.contains(course))
 			return false;
 
 		this.courses.add(course);
-		// this.schedule.addLectureHour(course.getSchedule());
-
 		return true;
 	}
 	@Override
